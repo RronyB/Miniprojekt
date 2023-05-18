c757a8a2980e8beebb4023b249f583e104e24015
RronyB <121437287+RronyB@users.noreply.github.com>
2023-05-18 10:51:42 +0200

    Add files via upload

---
+++ Miniprojekt 2/miniprojekt2.cs
@@ -0,0 +1,101 @@
+﻿using System;
+
+class Employee
+{
+    public int Id { get; set; }
+    public string Emri { get; set; } = string.Empty;
+    public string Pozita { get; set; } = string.Empty;
+    public double Rroga { get; set; }
+
+    public virtual double CalculateBonus(double rroga)
+    {
+        return 50000;
+    }
+}
+
+class Zhvillues : Employee
+{
+    public override double CalculateBonus(double rroga)
+    {
+        double perqindja = rroga * 0.2;
+        double bonusiFix = 50000;
+
+        return Math.Max(bonusiFix, perqindja);
+    }
+}
+
+class Menaxher : Employee
+{
+    public override double CalculateBonus(double rroga)
+    {
+        double perqindja = rroga * 0.25;
+        double bonusiFix = 50000;
+
+        return Math.Max(bonusiFix, perqindja);
+    }
+}
+
+class Admin : Employee
+{
+    public override double CalculateBonus(double rroga)
+    {
+        return 50000;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Shkruani te dhenat e puntorit:");
+
+        Console.Write("Emri: ");
+        string? emri = Console.ReadLine();
+
+        Console.Write("Pozita (Zhvillues/Menaxher/Admin): ");
+        string? pozita = Console.ReadLine();
+
+        Console.Write("Rroga: ");
+        string? rrogaInput = Console.ReadLine();
+        double rroga;
+
+        if (!double.TryParse(rrogaInput, out rroga))
+        {
+            Console.WriteLine("Vlera e rroges eshte invalide!");
+            return;
+        }
+
+        Employee employee;
+
+        if (pozita?.ToLower() == "zhvillues")
+        {
+            employee = new Zhvillues();
+        }
+        else if (pozita?.ToLower() == "menaxher")
+        {
+            employee = new Menaxher();
+        }
+        else if (pozita?.ToLower() == "admin")
+        {
+            employee = new Admin();
+        }
+        else
+        {
+            Console.WriteLine("Pozita eshte invalide!");
+            return;
+        }
+
+        employee.Id = 1;
+        employee.Emri = emri;
+        employee.Pozita = pozita;
+        employee.Rroga = rroga;
+
+        Console.WriteLine();
+
+        Console.WriteLine($"Puntori: {employee.Emri} ({employee.Pozita})");
+        Console.WriteLine($"Rroga: {employee.Rroga:C}");
+        Console.WriteLine($"Bonus: {employee.CalculateBonus(employee.Rroga):C}");
+
+        Console.ReadLine();
+    }
+}
---
+++ Miniprojekt1/miniprojekti1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+class Arrayminimum
+{
+    public int[] array;
+
+    public int minimum()
+    {
+        int minimum = array[0];
+        for (int i = 1; i < array.Length; i++)
+        {
+            if (array[i] < minimum)
+            {
+                minimum = array[i];
+            }
+        }
+        return minimum;
+    }
+
+    static void Main(string[] args)
+    {
+        Arrayminimum vargu = new Arrayminimum();
+        int gjatesia;
+
+        bool validInput = false;
+        do
+        {
+            Console.Write("Jepni gjatesine e vargut: ");
+            string input = Console.ReadLine();
+
+            if (int.TryParse(input, out gjatesia))
+            {
+                validInput = true;
+                vargu.array = new int[gjatesia];
+                for (int i = 0; i < gjatesia; i++)
+                {
+                    Console.Write($"Jepni madhesine e elementit {i + 1} ne varg: ");
+                    string hyrjeElement = Console.ReadLine();
+                    int element;
+                    if (int.TryParse(hyrjeElement, out element))
+                    {
+                        vargu.array[i] = element;
+                    }
+                    else
+                    {
+                        Console.WriteLine(
+                            "Ju lutemi jepni nje vlere numerike. Ju lutemi provoni perseri"
+                        );
+                        i--;
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("Ju lutemi jepni nje vlere numerike. Ju lutemi provoni perseri");
+            }
+        } while (!validInput);
+
+        int minimum = vargu.minimum();
+        Console.WriteLine($"Vlera minimale ne varg eshte: {minimum}");
+    }
+}
